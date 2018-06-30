ad60fcab522bf9cdaa080cc1bf982ef14f0482b4
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-06-30 13:56:11 +1000

    ADD: posts_index component renderPosts() helper function

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
+import _ from 'lodash';
 
 class PostsIndex extends Component {
   componentDidMount() {
     this.props.fetchPost();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
