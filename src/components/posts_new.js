4672097af1713fe81e7f20f09990821d54679a72
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-06-30 22:28:58 +1000

    ADD: form validation for posts_new component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,13 +11,23 @@ class PostsNew extends Component {
           type='text'
           {...field.input}
         />
+        {field.meta.touched ? field.meta.error : ''}
       </div>
     );
   }
 
+  onSubmit(values) {
+    console.log(values);
+    
+  }
+
   render() {
+    // es6 shorthand for const handleSubmit = this.props.handleSubmit
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      // have to bind this in handleSubmit because its a callback function
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label='Title'
           name='title'
@@ -33,10 +43,32 @@ class PostsNew extends Component {
           name='content'
           component={this.renderField}
         />
+        <button type='submit' className="btn btn-primary">Submit</button>
       </form>
     );}
 }
 
+function validate(values) {
+  // console.log(values) -> { title: 'asdf', categories: 'asdf', content: 'asdf';
+  const errors = {};
+
+  // Validate input from 'values'
+  if (!values.title) {
+    errors.title = 'Enter a title!';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories!';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content please!';
+  }
+
+  // if errors is empty, form is fine to submit
+  // if errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })( PostsNew );
